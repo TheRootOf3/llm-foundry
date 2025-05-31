6a59b9763d372a728aa63cde62feadb2a71b5584
Andrzej Szablewski <as3623@cam.ac.uk>
2025-05-31 02:17:59 +0100

    Implemented the first version of the Selective Multi Head Attention (SMHA).
    Signed-off-by: Andrzej Szablewski <as3623@cam.ac.uk>

--- llmfoundry/models/layers/attention.py
+++ llmfoundry/models/layers/attention.py
@@ -842,6 +842,358 @@ class GroupedQueryAttention(nn.Module):
         return extra_attn_kwargs
 
 
+@attention_classes.register_class("selective_multihead_attention")
+class SelectiveMultiHeadAttention(nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        n_heads: int,
+        attn_impl: str = "flash",
+        clip_qkv: Optional[float] = None,
+        qk_ln: bool = False,
+        qk_gn: bool = False,
+        fused_qkv: bool = True,
+        softmax_scale: Optional[float] = None,
+        attn_pdrop: float = 0.0,
+        norm_type: str = "low_precision_layernorm",
+        norm_eps: float = 1e-05,
+        fc_type: Optional[dict[str, Any]] = None,
+        device: Optional[str] = None,
+        bias: bool = True,
+        sliding_window_size: int = -1,
+        reuse_kv_layer_idx: Optional[int] = None,
+        attn_logit_softcapping: Optional[float] = None,
+        kv_dim: Optional[int] = None,
+    ):
+        super().__init__()
+
+        self.attn_impl = attn_impl
+        self.clip_qkv = clip_qkv
+        self.qk_ln = qk_ln
+        self.qk_gn = qk_gn
+        self.fused_qkv = fused_qkv
+
+        self.d_model = d_model
+        self.n_heads = n_heads
+        self.kv_n_heads = n_heads  # Selective MHA uses n_heads for kv_n_heads
+        self.sliding_window_size = sliding_window_size
+        self.reuse_kv_layer_idx = reuse_kv_layer_idx
+        self.attn_logit_softcapping = attn_logit_softcapping
+
+        self.kv_dim = kv_dim if kv_dim is not None else self.d_model
+        self.head_dim = d_model // n_heads
+
+        # Usually, fc_type dict should be passed in through MPTBlock's __init__ function.
+        if fc_type is None:
+            fc_type = copy.deepcopy(fc_type_defaults)
+            fc_type["bias"] = bias
+            fc_type["device"] = device
+        fc_type_name = fc_type["name"]
+
+        if qk_ln and qk_gn:
+            raise ValueError("Only one of qk_ln and qk_gn can be set to True.")
+
+        self.softmax_scale = softmax_scale
+        if self.softmax_scale is None:
+            self.softmax_scale = 1 / math.sqrt(self.d_model / self.n_heads)
+        self.attn_dropout_p = attn_pdrop
+
+        if self.reuse_kv_layer_idx is not None:
+            raise NotImplementedError
+        elif self.fused_qkv:
+            self.Wqkv = build_fc(
+                name=fc_type_name,
+                in_features=self.d_model,
+                out_features=self.d_model + 2 * self.kv_n_heads * self.head_dim,
+                fc_kwargs=fc_type,
+            )
+            # for param init fn; enables shape based init of fused layers
+            fuse_splits = [
+                i * self.head_dim for i in range(1, self.n_heads + 2 * self.kv_n_heads)
+            ]
+            self.Wqkv._fused = (0, fuse_splits)
+        else:
+            raise NotImplementedError
+
+        if self.qk_ln or self.qk_gn:
+            raise NotImplementedError
+
+        self.attn_fn = attention_implementations.get(self.attn_impl)
+
+        self.out_proj = build_fc(
+            name=fc_type_name,
+            in_features=self.d_model,
+            out_features=self.d_model,
+            fc_kwargs=fc_type,
+        )
+        self.out_proj._is_residual = True
+
+        # Selective multi-head attention params.
+
+        self.trainable_heads = [0, 1]
+
+    def forward(
+        self,
+        x: torch.Tensor,
+        past_key_value: Optional[tuple[torch.Tensor, torch.Tensor]] = None,
+        attn_bias: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        rotary_emb_w_meta_info: Optional[dict] = None,
+        is_causal: bool = True,
+        needs_weights: bool = False,
+        alibi_slopes: Optional[torch.Tensor] = None,
+        flash_attn_padding_info: Optional[dict[str, torch.Tensor]] = None,
+        prev_layer_key_value: Optional[tuple[torch.Tensor, torch.Tensor]] = None,
+        key_value_states: Optional[torch.Tensor] = None,
+    ) -> tuple[
+        torch.Tensor,
+        Optional[torch.Tensor],
+        Optional[tuple[torch.Tensor, torch.Tensor]],
+    ]:
+        extra_kwargs = {}
+        if prev_layer_key_value is not None:
+            extra_kwargs["prev_layer_key_value"] = prev_layer_key_value
+        query, key, value = self.get_qkv(
+            x=x,
+            key_value_states=key_value_states,
+            **extra_kwargs,
+        )
+
+        if rotary_emb_w_meta_info is not None:
+            query, key, value = self._apply_rotary_embeddings(
+                rotary_emb_w_meta_info,
+                query,
+                key,
+                value,
+            )
+
+        extra_attn_kwargs = self.get_implementation_specific_args(
+            attention_mask,
+            alibi_slopes,
+            flash_attn_padding_info,
+        )
+
+        context, attn_weights, past_key_value = self.attn_fn(
+            query,
+            key,
+            value,
+            n_heads=len(self.trainable_heads),
+            kv_n_heads=len(self.trainable_heads),
+            past_key_value=past_key_value,
+            softmax_scale=self.softmax_scale,
+            attn_bias=attn_bias,
+            is_causal=is_causal,
+            dropout_p=self.attn_dropout_p,
+            training=self.training,
+            needs_weights=needs_weights,
+            attn_logit_softcapping=self.attn_logit_softcapping,
+            sliding_window_size=self.sliding_window_size,
+            **extra_attn_kwargs,
+        )
+
+        trainable_o = torch.cat(
+            [
+                self.out_proj.weight[
+                    :,
+                    j * self.head_dim : (j + 1) * self.head_dim,
+                ]
+                for j in self.trainable_heads
+            ],
+            dim=-1,
+        )
+        output_t = context @ trainable_o.t()
+
+        if self.out_proj.bias is not None:
+            output_t += self.out_proj.bias
+
+        return output_t, attn_weights, past_key_value
+
+    def get_qkv(
+        self,
+        x: torch.Tensor,
+        prev_layer_key_value: Optional[tuple[torch.Tensor, torch.Tensor]] = None,
+        key_value_states: Optional[torch.Tensor] = None,
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Computes and returns the query, key, and value tensors.
+
+        Args:
+            x (torch.Tensor): The input query tensor.
+            prev_layer_key_value  (Optional[Tuple[torch.Tensor, torch.Tensor]]): The key value of the previous layer.
+            key_value_states (Optional[torch.Tensor]): The input tensor for keys and values.
+
+        Returns:
+            query (torch.Tensor): The query tensor.
+            key (torch.Tensor): The key tensor.
+            value (torch.Tensor): The value tensor.
+        """
+        if self.reuse_kv_layer_idx is not None:
+            raise NotImplementedError
+
+        if self.fused_qkv:
+            if key_value_states is not None:
+                raise ValueError(
+                    "Cannot use separate hidden and key_value states when fused_qkv = True.",
+                )
+
+            # This is the place to separate the computation.
+
+            # Original line
+            # qkv = self.Wqkv(x)
+
+            # Selective computation
+            trainable_w = torch.cat(
+                [
+                    self.Wqkv.weight[
+                        i * self.d_model
+                        + j * self.head_dim : i * self.d_model
+                        + (j + 1) * self.head_dim,
+                        :,
+                    ]
+                    for j in self.trainable_heads
+                    for i in range(3)
+                ]
+            )
+
+            qkv = x @ trainable_w.t()
+
+            if self.Wqkv.bias is not None:
+                trainable_b = torch.cat(
+                    [
+                        self.Wqkv.bias[
+                            i * self.d_model
+                            + j * self.head_dim : i * self.d_model
+                            + (j + 1) * self.head_dim,
+                        ]
+                        for j in self.trainable_heads
+                        for i in range(3)
+                    ]
+                )
+                qkv += trainable_b
+
+            if self.clip_qkv:
+                qkv = qkv.clamp(min=-self.clip_qkv, max=self.clip_qkv)
+
+            query, key, value = qkv.split(
+                len(self.trainable_heads) * self.head_dim,
+                dim=2,
+            )
+        else:
+            raise NotImplementedError
+
+        if self.qk_ln or self.qk_gn:
+            raise NotImplementedError
+
+        return query, key, value
+
+    def _apply_rotary_embeddings(
+        self,
+        rotary_emb_w_meta_info: dict[str, Any],
+        query: torch.Tensor,
+        key: torch.Tensor,
+        value: torch.Tensor,
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        if self.reuse_kv_layer_idx is not None:
+            orig_key, orig_value = key, value
+            key, value = torch.empty_like(key), torch.empty_like(value)
+
+        rotary_emb = rotary_emb_w_meta_info["rotary_emb"]
+        seq_len = rotary_emb_w_meta_info["seq_len"]
+        offset_info = rotary_emb_w_meta_info["offset_info"]
+        bsz, seqlen = query.shape[:2]
+        query = query.view(bsz, seqlen, -1, self.head_dim)
+        key = key.view(bsz, seqlen, -1, self.head_dim)
+
+        if rotary_emb_w_meta_info["impl"] == "dail":
+            value = value.view(bsz, seqlen, -1, self.head_dim)
+
+            kv = torch.stack([key, value], dim=2)
+            # Note: Rotates in place (https://github.com/Dao-AILab/flash-attention/blob/320fb59487658f033f56711efd3d61b7c7a6f8f3/flash_attn/layers/rotary.py#L429)
+            query, kv = rotary_emb(
+                query,
+                kv,
+                seqlen_offset=offset_info,
+                max_seqlen=seq_len,
+            )
+            [key, value] = torch.unbind(kv, dim=2)
+
+            value = value.view(bsz, seqlen, -1)
+        elif rotary_emb_w_meta_info["impl"] == "hf":
+            if is_transformers_version_gte("4.38"):
+                (cos, sin) = rotary_emb(
+                    x=value,
+                    position_ids=offset_info,
+                )
+            else:
+                (cos, sin) = rotary_emb(x=value, seq_len=seq_len)
+            if is_transformers_version_gte("4.38"):
+                # In the following lines we move the cos and sin tensors to the same devices as query. These operations should be no-ops during training.
+                # This is done to fix pipeline parallel generation using hf.generate. Please see this comment for details: https://github.com/mosaicml/llm-foundry/pull/1332#issue-2386827204
+                cos = cos.to(query.device)
+                sin = sin.to(query.device)
+                query, key = apply_rotary_pos_emb(
+                    q=query,
+                    k=key,
+                    cos=cos,
+                    sin=sin,
+                    position_ids=None,
+                    unsqueeze_dim=2,
+                )
+            elif is_transformers_version_gte("4.36"):
+                query, key = apply_rotary_pos_emb(
+                    q=query,
+                    k=key,
+                    cos=cos,
+                    sin=sin,
+                    position_ids=offset_info,
+                    unsqueeze_dim=2,
+                )
+            else:
+                query = query.transpose(1, 2)
+                key = key.transpose(1, 2)
+                query, key = apply_rotary_pos_emb(
+                    q=query,
+                    k=key,
+                    cos=cos,
+                    sin=sin,
+                    position_ids=offset_info,
+                )
+                query = query.transpose(1, 2)
+                key = key.transpose(1, 2)
+
+        query = query.view(bsz, seqlen, -1)
+        key = key.view(bsz, seqlen, -1)
+        if self.reuse_kv_layer_idx is not None:
+            return query, orig_key, orig_value  # type: ignore
+        return query, key, value
+
+    def get_implementation_specific_args(
+        self,
+        attention_mask: Optional[torch.Tensor] = None,
+        alibi_slopes: Optional[torch.Tensor] = None,
+        flash_attn_padding_info: Optional[dict[str, torch.Tensor]] = None,
+    ) -> dict[str, Any]:
+        """Returns attention implementation specific args.
+
+        Args:
+            attention_mask (Optional[torch.Tensor]): The attention mask.
+            alibi_slopes (Optional[torch.Tensor]): The alibi slopes.
+            flash_attn_padding_info (Optional[dict[str, torch.Tensor]]): The padding information, only required for flash attention.
+
+        Returns:
+            extra_attn_kwargs (dict[str, Any]): Implementation specific args.
+        """
+        if self.attn_impl == "flash":
+            extra_attn_kwargs = {
+                "should_repeat_kv_for_gqa": not is_flash_v2_installed(),
+                "alibi_slopes": alibi_slopes,
+                "flash_attn_padding_info": flash_attn_padding_info,
+                "key_padding_mask": None,
+            }
+        else:
+            extra_attn_kwargs = {"key_padding_mask": attention_mask}
+        return extra_attn_kwargs
+
+
 @attention_classes.register_class('multihead_attention')
 class MultiheadAttention(GroupedQueryAttention):
     """Multi-head self attention.
